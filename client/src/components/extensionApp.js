171d7984680b2a7181397f1356534b7442bae918
Vishal <superdhawan@gmail.com>
2019-03-30 09:50:02 +0530

    added link to extension repo

--- client/src/components/extensionApp.js
+++ client/src/components/extensionApp.js
@@ -4,7 +4,7 @@ class ExtensionApp extends Component{
     render()
     {
         return(<div className='extensionApp'>
-            <h1>Install the <a href='#LinkToExtension'>FOCUS</a> Chrome Extension</h1>
+            <h1>Install the <a href='https://github.com/XMIRTUNJAY/focus-extension'>FOCUS</a> Chrome Extension</h1>
             <img className='extensionImg' src={extensionExample} alt="Chrome Extension"/>
             <h1>Download the <a href='#LinkToApp'>Android App</a></h1>
         </div>)
